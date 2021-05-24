c8082f5f12bcb04df9bf5a99ce28d7aa84838d20
kirillNovoseletskii <48125494+kirillNovoseletskii@users.noreply.github.com>
2021-05-24 10:12:26 +0300

    Create Transaction.go

---
+++ pkg/block/Transaction.go
@@ -0,0 +1,66 @@
+package block
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+	"log"
+)
+
+type Transaction struct {
+	ID 				[]byte
+	TxInputs	[]TxInput
+	TxOutputs []TxOutput 
+}
+
+type TxOutput struct {
+	Value  int
+	PubKey string
+}
+
+type TxInput struct {
+	ID []byte
+	Out int
+	Sig string
+}
+
+func (inp *TxInput) CanUnlock(data string) bool {
+	return inp.Sig == data
+}
+
+func (out *TxOutput) CanBeUnlocked(data string) bool {
+	return out.PubKey == data
+}
+
+func (tx *Transaction) IsCoinbase() bool {
+	return len(tx.TxInputs) == 1 && len(tx.TxInputs[0].ID) == 0 && tx.TxInputs[0].Out == -1 
+}
+
+func (tx *Transaction) SetID() {
+	var encoded bytes.Buffer
+	var hash [32]byte
+
+	encode := gob.NewEncoder(&encoded)
+	err := encode.Encode(tx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	hash = sha256.Sum256(encoded.Bytes())
+	tx.ID = hash[:]
+}
+
+func CoinbaseTx(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Coin to %s", to)
+	}
+
+	txin := TxInput{[]byte{}, -1, data}
+	txout := TxOutput{100, to}
+
+	tx := Transaction{nil, []TxInput{txin}, []TxOutput{txout}}
+	tx.SetID()
+
+	return &tx
+}
